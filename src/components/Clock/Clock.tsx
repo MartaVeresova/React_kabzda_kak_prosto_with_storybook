c80dc1e4f54265add355f9a8eebbfc4ed7e1b4e7
MartaVeresova <MartaVeresovaa@gmail.com>
2021-06-16 14:32:32 +0300

    useEffect - Clock; AnalogClock

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import {DigitalClockView} from './DigitalClockView';
+import {AnalogClockView} from './AnalogClockView';
+
+type PropsType = {
+    mode?: 'analog' | 'digital'
+}
+
+export type ClockViewPropsType = {
+    date: Date
+}
+
+
+export const Clock: React.FC<PropsType> = (props) => {
+
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            console.log('TICK')
+            setDate(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+
+
+    let view
+    switch (props.mode) {
+
+        case 'analog':
+            view = <AnalogClockView date={date}/>
+            break
+
+        case 'digital':
+        default:
+            view = <DigitalClockView date={date}/>
+    }
+
+    return (
+        <div>
+            {view}
+        </div>
+    )
+}
+
+
+
